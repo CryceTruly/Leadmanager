dff41e98ec2ff34add2f20fac0d5e5f0a18624ce
crycetruly <crycetruly@gmail.com>
2019-02-01 11:18:22 +0300

    Added Lead Model

--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Lead(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField(max_length=255,unique=True)
+    message=models.CharField(max_length=5000,blank=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    

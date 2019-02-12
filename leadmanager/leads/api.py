f2b52ad8cb503574b5e7928e27760e5501b6130b
crycetruly <crycetruly@gmail.com>
2019-02-12 22:45:38 +0300

    Wrote user routes

--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -7,6 +7,12 @@ from leads.serializers import LeadSerializer
 class LeadViewSet(viewsets.ModelViewSet):        
     queryset=Lead.objects.all()
     permission_classes=[
-        permissions.AllowAny
+        permissions.IsAuthenticated
     ]
     serializer_class=LeadSerializer
+    def get_queryset(self):
+        return self.request.user.leads.all()
+
+    def perfom_create(self,serializer):
+        serializer.save(owner=self.request.user)
+    

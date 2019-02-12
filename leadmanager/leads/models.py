af966b9f4f6afe98de5149a6e59f5b1b737253f8
crycetruly <crycetruly@gmail.com>
2019-02-12 22:44:04 +0300

    Adds owner to lead

--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Lead(models.Model):
@@ -6,4 +7,5 @@ class Lead(models.Model):
     email=models.EmailField(max_length=255,unique=True)
     message=models.CharField(max_length=5000,blank=True)
     created_at=models.DateTimeField(auto_now_add=True)
+    owner=models.ForeignKey(User,related_name='leads',on_delete=models.CASCADE,null=True)
     
